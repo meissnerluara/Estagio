6f53b2483a4ab3ea0965ae355be7ec30c404dc2e
meissnerluara <luara.godoy@icloud.com>
2025-08-08 19:31:58 -0300

    Atualizando validateRequest

--- patologia-backend/src/middlewares/validateRequest.js
+++ patologia-backend/src/middlewares/validateRequest.js
@@ -2,7 +2,7 @@ const { HttpError } = require('../utils/HttpError');
 
 // middleware para validar requisições usando um schema joi
 const validateRequest = (schema, property = 'body') => {
-  return (req, next) => {
+  return (req, res, next) => {
     console.log('Validando requisição...');
     console.log('Dados recebidos:', req[property]);
 
